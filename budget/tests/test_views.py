2db56611070d135325f177ad72c630ef508af9ab
Abmael Bandeira <abmaelbandeira@hotmail.com>
2021-09-16 00:36:23 -0300

    Cria testes de POST no data em project_detail

--- budget/tests/test_views.py
+++ budget/tests/test_views.py
@@ -62,3 +62,29 @@ class TestViews(TestCase):
 
         self.assertEquals(response.status_code, 302)
         self.assertEquals(self.project1.expenses.first().title, 'expense1')
+
+
+    def test_project_detail_POST_no_data(self):
+        response = self.client.post(self.detail_url)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(self.project1.expenses.count(), 0)
+
+
+    def test_project_detail_DELETE_deletes_expense(self):
+        category1 = Category.objects.create(
+            project=self.project1,
+            name='development'
+        )
+        Expense.objects.create(
+            project=self.project1,
+            title='expense1',
+            amount=1000,
+            category=category1
+        )
+
+        response = self.client.delete(self.detail_url, json.dumps({
+            'id': 1
+        }))
+
+        self.assertEquals(response.status_code, 204)
+        self.assertEquals(self.project1.expenses.count(), 0)
