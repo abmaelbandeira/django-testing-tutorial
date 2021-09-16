16d1af22659ab42f5313894d3bcd8e7ed6ba2fc3
Abmael Bandeira <abmaelbandeira@hotmail.com>
2021-09-16 00:27:59 -0300

    Cria teste de view teste_project_detail_POST_adds_new_expense

--- budget/tests/test_views.py
+++ budget/tests/test_views.py
@@ -17,22 +17,48 @@ class TestViews(TestCase):
             budget=10000
         )
 
-    def test_list_view_retorna_status_correto(self):
+
+    def test_project_list_view_retorna_status_correto(self):
         response = self.client.get(self.list_url)
         self.assertEquals(response.status_code, 200)
 
-    def test_list_view_renderiza_template_correto(self):
+
+    def test_project_list_view_renderiza_template_correto(self):
         response = self.client.get(self.list_url)
         self.assertTemplateUsed(response, 'budget/project-list.html')
 
-    def test_create_view_retorna_status_correto(self):
+
+    def test_project_create_view_retorna_status_correto(self):
         response = self.client.get(self.add_url)
         self.assertEquals(response.status_code, 200)
 
-    def test_create_view_renderiza_template_correto(self):
+
+    def test_project_create_view_renderiza_template_correto(self):
         response = self.client.get(self.add_url)
         self.assertTemplateUsed(response, 'budget/add-project.html')
 
-    def test_detail_view_retorna_status_correto(self):
+
+    def test_project_detail_view_retorna_status_correto_GET(self):
         response = self.client.get(self.detail_url)
         self.assertEquals(response.status_code, 200)
+
+
+    def test_project_detail_view_renderiza_template_correto_GET(self):
+        response = self.client.get(self.detail_url)
+        self.assertTemplateUsed(response, 'budget/project-detail.html')
+
+
+    def teste_project_detail_POST_adds_new_expense(self):
+        Category.objects.create(
+            project=self.project1,
+            name='development'
+        )
+
+        response = self.client.post(self.detail_url, {
+            'title': 'expense1',
+            'amount': 1000,
+            'category': 'development'
+        })
+
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(self.project1.expenses.first().title, 'expense1')
