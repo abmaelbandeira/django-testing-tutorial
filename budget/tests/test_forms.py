51c3568aa5d0f89a4c76d323733e149fed7a851f
Abmael Bandeira <abmaelbandeira@hotmail.com>
2021-09-16 10:39:52 -0300

    Cria teste de formulário de expense que verifica se o formulário não foi preenchido e se a quantidade de erros foi 3 (campos sem dados)

--- budget/tests/test_forms.py
+++ budget/tests/test_forms.py
@@ -11,3 +11,9 @@ class TestForms(SimpleTestCase):
             'category': 'development'
         })
         self.assertTrue(form.is_valid())
+
+    def test_expense_form_no_data(self):
+        form = ExpenseForm(data={})
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 3)
